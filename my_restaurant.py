0df3c4fef477affdcf852cb109662479af776655
xiaoyiming2018 <41030689+xiaoyiming2018@users.noreply.github.com>
2018-11-29 11:37:28 +0800

    my_restaurant
    my_restaurant

---
+++ my_restaurant.py
@@ -0,0 +1,9 @@
+from random import randint
+
+from restaurant import Restaurant, IceCreamStand
+
+# meal = IceCreamStand('哈根达斯', '冰淇淋',['草莓', '巧克力', '芒果'])
+# meal.show_flavors()
+# meal.set_number_served(20)
+while True:
+    print(randint(1,6))
